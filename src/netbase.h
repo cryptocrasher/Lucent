b872b4cd5db9f82fa4f982aba113e75bab4bd9b4
Crasher <36900605+cryptocrasher@users.noreply.github.com>
2018-08-29 11:14:55 -0400

    Update netbase.h

--- src/netbase.h
+++ src/netbase.h
@@ -22,7 +22,7 @@ extern bool fNameLookup;
 /** -timeout default */
 static const int DEFAULT_CONNECT_TIMEOUT = 5000;
 //! -dns default
-static const int DEFAULT_NAME_LOOKUP = true;
+static const int DEFAULT_NAME_LOOKUP = false;
 
 #ifdef WIN32
 // In MSVC, this is defined as a macro, undefine it to prevent a compile and link error
